5c79a29ad083c12d55145d9dcde4be18f11fbc73
UO277876 <79647031+UO277876@users.noreply.github.com>
2022-04-02 02:01:39 +0200

    Implementacion login con pod v1

---
+++ webapp/src/components/Login/ProfilerLogIn.tsx
@@ -0,0 +1,36 @@
+import { useSession, CombinedDataProvider, Image, LogoutButton, Text } from "@inrupt/solid-ui-react";
+import { Button, Card, CardActionArea, CardContent, Container, Typography } from "@material-ui/core";
+import { FOAF, VCARD } from "@inrupt/lit-generated-vocab-common";
+
+const ProfilerLogIn = () => {
+  const { session } = useSession();
+  const { webId } = session.info;
+
+  return (
+    <Container fixed>
+      <CombinedDataProvider datasetUrl={webId} thingUrl={webId}>
+        <Card style={{ maxWidth: 480 }}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              <Text property={FOAF.name.iri.value} />
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p" style={{ display: "flex", alignItems: "center" }}>
+              <Text property={VCARD.organization_name.iri.value} />
+            </Typography>
+          </CardContent>
+
+          <CardActionArea style={{ justifyContent: "center", display: "flex" }}>
+            <Image property={VCARD.hasPhoto.iri.value} width={480} />
+          </CardActionArea>
+        </Card>
+      </CombinedDataProvider>
+      
+      <LogoutButton >
+        <Button style={{ marginTop: 20 }}  variant="contained" color="primary">
+          Logout
+        </Button>
+      </LogoutButton>
+    </Container>
+  );
+}
+export default ProfilerLogIn;
