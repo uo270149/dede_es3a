1d611ba15fa2d0e811cf4d40fd31ba82330a5010
UO271447 <UO271447@uniovi.es>
2022-04-02 19:26:30 +0200

    añadiendo logOut + css Perfil

--- webapp/src/components/Login/ProfileViewer.tsx
+++ webapp/src/components/Login/ProfileViewer.tsx
@@ -1,20 +1,51 @@
 import { useSession, CombinedDataProvider, Image, LogoutButton, Text } from "@inrupt/solid-ui-react";
-import { Button, Card, CardActionArea, CardContent, Container, Typography } from "@material-ui/core";
+import { Button, Card, CardActionArea, CardContent, CardHeader, Container, Typography } from "@material-ui/core";
 import { FOAF, VCARD } from "@inrupt/lit-generated-vocab-common";
 import Nav from '../Fragments/Nav';
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: 'flex', 
+      flexWrap: 'wrap',
+      width: '60%',
+      height:'60%',
+      marginTop:'10%',
+      marginLeft:'30%',
+      margin: `${theme.spacing(0)} auto`
+    },
+    loginBtn: {
+      marginTop: theme.spacing(2),
+      flexGrow: 1
+    },
+    header: {
+      textAlign: 'center',
+      background: '#212121',
+      color: '#fff'
+    },
+    card: {
+      marginTop: theme.spacing(10),
+      width: '70%',
+      height:''
+    },
+  })
+);
 const ProfileViewer = () => {
+  const classes = useStyles();
   const { session } = useSession();
   const { webId } = session.info;
 
   return (
     <><Nav />
+    <form className={classes.container} noValidate autoComplete="on">
     <Container fixed>
       {session.info.webId ? (
         <CombinedDataProvider
           datasetUrl={session.info.webId}
           thingUrl={session.info.webId}>
-          <Card style={{ maxWidth: 480 }}>
+          <Card className={classes.card}>
+          <CardHeader className={classes.header} title="User" />
             <CardContent>
               <Typography gutterBottom variant="h5" component="h2">
                 <Text property={FOAF.name.iri.value} />
@@ -31,14 +62,13 @@ const ProfileViewer = () => {
         </CombinedDataProvider>
       ) : null}
       <LogoutButton>
-        <Button style={{ marginTop: 20 }} variant="contained" color="primary">
+        <Button style={{ marginTop: 20 }} variant="contained" color="primary" href="/FormLogIn">
           Logout
         </Button>
       </LogoutButton>
-      <Button href="http://localhost:3000">
-          Aceptar
-        </Button>
-    </Container></>
+
+    </Container>
+    </form></>
   );
 }
 
