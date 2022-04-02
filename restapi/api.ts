832dac2f38fda2e41dd2c3f80953b309efe5cf4c
Sofía Suárez Fernández <UO270149@uniovi.es>
2022-04-02 17:56:36 +0200

    creación archivo api.ts en restapi y algunos test de usuarios

---
+++ restapi/api.ts
@@ -0,0 +1,6 @@
+import express, { Request, Response, Router } from 'express';
+import { check } from 'express-validator';
+
+const api: Router = express.Router();
+
+export default api;
---
+++ restapi/tests/api.test.ts
@@ -0,0 +1,51 @@
+import request, { Response } from 'supertest';
+import express, { Application } from 'express';
+import * as http from 'http';
+import bp from 'body-parser';
+import cors from 'cors';
+import exp from 'constants';
+import api from '../api';
+
+let app: Application;
+let server: http.Server;
+
+beforeAll(async () => {
+    app = express();
+    const port: number = 5000;
+    const options: cors.CorsOptions = {
+        origin: ['http://localhost:3000']
+    };
+    app.use(cors(options));
+    app.use(bp.json());
+    app.use("/api", api);
+
+    server = app.listen(port, (): void => {
+        console.log('Servidor Restapi para testing escuchando en ' + port);
+    }).on("error", (error: Error) => {
+        console.error('Error ocurrido: ' + error.message);
+    })
+});
+
+afterAll(async () => {
+    server.close(); // cerrar el servidor
+});
+
+describe('user ', () => {
+    /**
+     * Probar que podemos listar usuarios sin errores
+     */
+    it('can be listed', async () => {
+        const response: Response = await request(app).get("api/users/list");
+        expect(response.statusCode).toBe(200);
+    });
+
+    /**
+     * Probar que un usuario puede ser creado a través de productService sin lanzar errores
+     */
+    it('can be created correctly', async () => {
+        let username: string = 'Pablo';
+        let email: string = 'gonzalezgpablo@uniovi.es';
+        const response: Response = await request(app).post('/api/users/add').send({ name: username, email: email }).set('Accept', 'application/json');
+        expect(response.statusCode).toBe(200);
+    })
+});
