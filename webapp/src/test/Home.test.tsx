89318253840f7b2211b283918c132e12efd9f860
UO277876 <79647031+UO277876@users.noreply.github.com>
2022-04-23 19:18:57 +0200

    intento de solucion del error

--- webapp/src/test/Home.test.tsx
+++ webapp/src/test/Home.test.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import { render } from "@testing-library/react";
-import Home from '../components/Home/Shoes';
-import { ListProduct, TypeProduct } from '../shared/shareddtypes';
+import { TypeProduct } from '../shared/shareddtypes';
 import ShoesView from '../components/Home/Shoes';
+import mongoose from 'mongoose';
+
+const { ObjectId } = require('mongodb');
 
 test('check that the home is rendering propertly', async() => {
     const products: TypeProduct[] = [{
-        _objectId: ObjectId;
-        id: string;
-        nombre: string;
-        precio: number;
-        descripcion: string;
-        imagen: string;
+        _objectId: ObjectId("62598df82841d14b30fbd6b1"),
+        id: "1b",
+        imagen: "https://i.imgur.com/dHfRXIu.jpg",
+        nombre: "Adidas Pixar",
+        precio: 119.99,
+        descripcion: "Zapatilla con personajes de Pixar animacion"
     }];
     
-    const { getByText } = render(<ShoesView parsed={products}/);
-    expect(getByText(products[0].name)).toBeInTheDocument();
+    const { getByText } = render(<ShoesView products={ products }/>);
+    expect(getByText(products[0].nombre)).toBeInTheDocument();
     expect(getByText(products[0].precio)).toBeInTheDocument();
 });
-
-test('Lista de Home rendering', async() => {
-    const listProducts: ListProduct[] = [{img:'https://i.imgur.com/VwtfVgU.jpg', title:'Zapatillas Baby Yoda', author: '@rollelflex_graphy726'}];
-    
-    const { getByText } = render();
-    expect(getByText(listProducts[0].img)).toBeInTheDocument();
-    expect(getByText(listProducts[0].title)).toBeInTheDocument();
-})
