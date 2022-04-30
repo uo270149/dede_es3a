08c22ecc2610c06b7081744e0695715a0fb644a4
UO271447 <UO271447@uniovi.es>
2022-05-01 01:32:54 +0200

    Corrigiendo imagenes de details

--- webapp/src/test/Details/LeftDetails.test.tsx
+++ webapp/src/test/Details/LeftDetails.test.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { screen, render } from "@testing-library/react";
 import LeftDetails from '../../components/Details/LeftDetails';
-
+import {ObjectId} from 'bson';
+import { TypeProduct } from '../../shared/shareddtypes';
 test('Left Details funcionando', async() => {
+    const strObject : ObjectId = new ObjectId("62598df82841d14b30fbd6b1");
+    const product: TypeProduct[] = [{
+        _objectId: strObject,
+        id: "1b",
+        imagen: "https://i.imgur.com/dHfRXIu.jpg",
+        nombre: "Adidas Pixar",
+        precio: 119.99,
+        descripcion: "Zapatilla con personajes de Pixar animacion"
+    }];
 
-    render(<LeftDetails/>);
+
+    render(<LeftDetails product={product}/>);
 
     const deLado1 = screen.getByAltText("De lado 1");
     expect(deLado1).toBeInTheDocument();
