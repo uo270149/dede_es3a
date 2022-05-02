8dfc9672cb0c8cb3a6d22f510c1592b236baa915
UO277876 <79647031+UO277876@users.noreply.github.com>
2022-05-02 17:09:14 +0200

    coverage

--- restapi/server.ts
+++ restapi/server.ts
@@ -1,5 +1,6 @@
-import express, {Request, Response} from 'express'; 
+import express from 'express'; 
 import mongoose from 'mongoose';
+import cors from "cors";
 import { json } from 'body-parser';
 import config from './config'
 import { productoRouter } from './rutas/productoRutas';
@@ -8,10 +9,11 @@ import { pedidoRouter } from './rutas/pedidoRutas';
 import http from 'http';
 import "dotenv/config";
 import path from "path";
+
 const app = express()
 
-
 app.use(json())
+app.use(cors())
 
 app.use(productoRouter)
 app.use(usuarioRouter)
